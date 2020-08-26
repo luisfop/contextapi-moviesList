333e327349297a3184b71ae3952b6aa9d5351d15
luisop <luisfop77@gmail.com>
2020-08-26 13:37:19 -0300

    initial commit, added movielist and movie components

---
+++ src/components/movieList/MovieList.js
@@ -0,0 +1,41 @@
+import React,{useState} from 'react';
+
+import Movie from '../movie/Movie';
+
+
+const MovieList = () => {
+
+    const [movies, setMovies ] = useState([
+        {
+            name: 'Star Wars',
+            price: 'R$: 99,90',
+            id: 13212
+        },
+        {
+            name: 'Lord of the Rings',
+            price: 'R$: 59,90',
+            id: 23211
+        },
+        {
+            name: 'Into the Wild - Na natureza selvagem',
+            price: 'R$: 49,90',
+            id: 34777
+        }
+    ]);
+    
+
+
+
+
+    return(
+        <div>
+            {movies.map(movie => (
+                <Movie name={movie.name} />
+            ))}
+        </div>
+    )
+
+}
+
+
+export default MovieList;
